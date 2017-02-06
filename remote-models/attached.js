f41a0cca208b8d3b88b48aeff5f332b2e01f54dd
kimsangsang <teeth1231@naver.com>
2017-02-06 14:19:48 +0900

    - 비즈리스트, 디테일 추가
    Signed-off-by: kimsangsang <teeth1231@naver.com>

---
+++ remote-models/attached.js
@@ -0,0 +1,38 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('attached', {
+    ID: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    group: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    file_path: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    file_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    meta_value: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    tableName: 'attached'
+  });
+};
