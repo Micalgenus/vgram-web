327a18d64f379bc87bfc96bd8689decd1148fde2
tjrdnjs1609 <30388690+tjrdnjs1609@users.noreply.github.com>
2017-09-18 21:28:49 +0900

    log -> logger이름변경 버그 수정

--- tests/data/user_user_relationship.js
+++ tests/data/user_user_relationship.js
@@ -10,68 +10,68 @@ var postData = require('./post');
 // user_user_relationship table
 //------------------------------------------
 let relationdata = [
-    //    {
-    //       user_id: 1,
-    //       user_target_id: 2
-    //    },
-    //    {
-    //       user_id: 1,
-    //       user_target_id: 3
-    //    },
-    //    {
-    //       user_id: 1,
-    //       user_target_id: 4
-    //    },
-    //    {
-    //       user_id: 2,
-    //       user_target_id: 1
-    //    },
-    //    {
-    //       user_id: 2,
-    //       user_target_id: 3
-    //    },
-    //    {
-    //       user_id: 3,
-    //       user_target_id: 4
-    //    },
-    //    {
-    //       user_id: 4,
-    //       user_target_id: 5
-    //    },
-    //    {
-    //       user_id: 4,
-    //       user_target_id: 6
-    //    },
-    //    {
-    //       user_id: 4,
-    //       user_target_id: 7
-    //    },
-    //    {
-    //       user_id: 5,
-    //       user_target_id: 7
-    //    },
-    //    {
-    //       user_id: 6,
-    //       user_target_id: 1
-    //    },
-    //    {
-    //       user_id: 7,
-    //       user_target_id: 2
-    //    }
+       {
+          user_id: 1,
+          user_target_id: 2
+       },
+       {
+          user_id: 1,
+          user_target_id: 3
+       },
+       {
+          user_id: 1,
+          user_target_id: 4
+       },
+       {
+          user_id: 2,
+          user_target_id: 1
+       },
+       {
+          user_id: 2,
+          user_target_id: 3
+       },
+       {
+          user_id: 3,
+          user_target_id: 4
+       },
+       {
+          user_id: 4,
+          user_target_id: 5
+       },
+       {
+          user_id: 4,
+          user_target_id: 6
+       },
+       {
+          user_id: 4,
+          user_target_id: 7
+       },
+       {
+          user_id: 5,
+          user_target_id: 7
+       },
+       {
+          user_id: 6,
+          user_target_id: 1
+       },
+       {
+          user_id: 7,
+          user_target_id: 2
+       }
 ]
-relationdata = _.map(userData, function (user, i) {
+// relationdata = _.map(userData, function (user, i) {
   
-    for (var user of userData) {
+//     for (var user of userData) {
         
-        return {
-            user_id: user.ID,
-            user_target_id: user.ID+1
-        };
-        // if (!_.find(data, tmp)) {
-        //     data.push(tmp);
-        // }
-    }
-});
+//         return {
+//             user_id: user.ID,
+//             user_target_id: user.ID+1
+//         };
+//         // if (!_.find(data, tmp)) {
+//         //     data.push(tmp);
+//         // }
+//     }
+// });
 
 
 module.exports = relationdata;
